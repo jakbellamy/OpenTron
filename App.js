64421371c4ebba7c800d4555eac2891cddcb6465
Jakob Bellamy <jakbellamy@gmail.com>
2019-04-13 15:47:05 -0500

    camera support

--- App.js
+++ App.js
@@ -1,17 +1,51 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import { Camera, Permissions } from 'expo';
 
 export default class App extends React.Component {
   
   state = {
-    answers: []
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.back,
+  }
+
+  async componentDidMount() {
+    const permission = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: permission.status === 'granted' });
   }
 
   render() {
     return (
       <>
-        <View style={styles.container}>
-          <Text>Is it still working?</Text>
+        <View style={{ flex: 1 }}>
+        { this.state.hasCameraPermission &&
+          <Camera style={{ flex: 1 }} type={this.state.type}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                flexDirection: 'row',
+              }}>
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                }}
+                onPress={() => {
+                  this.setState({
+                    type: this.state.type === Camera.Constants.Type.back
+                      ? Camera.Constants.Type.front
+                      : Camera.Constants.Type.back,
+                  });
+                }}>
+                <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                  {' '}Flip{' '}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </Camera>
+        }
         </View>
       </>
     );
