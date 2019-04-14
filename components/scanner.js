1822538093d34dd4c7008532ee8c53063736cff8
Jakob Bellamy <jakbellamy@gmail.com>
2019-04-14 00:35:07 -0500

    ughh

--- components/scanner.js
+++ components/scanner.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
-import { Camera, Permissions } from 'expo';
-
+import { Camera, Permissions, ImagePicker } from 'expo';
 
 export default class Scanner extends React.Component {
   
@@ -17,81 +16,18 @@ export default class Scanner extends React.Component {
     }
 
     options = {
-      base64: true,
-      onPictureSaved: function(data){
-        console.log(data)
-      }
+      base64: true
     }
-
-  //   async scan() {
-  //     console.log('Button Pressed')
-  //     console.log('Taking photo')
-  //     if(this.camera){
-  //       let photo = await this.camera.takePictureAsync()
-  //       photo.then((result) => {
-  //         console.log(result)
-  //       }, (err) => {
-  //         console.log(err)
-  //       })
-  //       console.log(photo);
-  //     }
-  // }
-
-  async accessCamera()
-  {
-    if (this.state.isCapturing)
-    {
-        let photo = await this.camera.takePictureAsync();
-        this.setState({ isCapturing: false, accessCameraLabel: 'Retake', capturedPhoto: photo.uri});
-    }
-    else
-    {
-          this.setState({ isCapturing: true, accessCameraLabel: 'Capture', capturedPhoto: null});
-    }
-  }
-  
+    
     render() {
+      ImagePicker.launchCameraAsync(this.options)
+      .then((success) => {
+        console.log("success: "+ success)
+      }, (err) => {
+        console.log("error: " + err)
+      })
       return (
-          <View style={{ flex: 1 }}>
-                  {this.state.hasCameraPermission &&
-            <Camera style={{ flex: 1 }} type={this.state.type} >
-              <View
-                style={{
-                  flex: 1,
-                  backgroundColor: 'transparent',
-                  flexDirection: 'row',
-                }}>
-                <TouchableOpacity
-                  style={{
-                    flex: 0.2,
-                    alignSelf: 'flex-end',
-                    alignItems: 'center',
-                  }}
-                  onPress={() => {
-                    this.props.menu()
-                  }
-                  }>
-                          <Text style={{ fontSize: 18, marginBottom: 10, color: 'white', width: 50, backgroundColor: '#000' }}>
-                    {' '}Menu{' '}
-                  </Text>
-               </TouchableOpacity>
-                <TouchableOpacity
-                    style={{
-                        flex: 1.2,
-                        alignSelf: 'flex-end',
-                        alignItems: 'center',
-                    }}
-                    onPress={() => {this.accessCamera()}}
-                >
-                          <Text style={{ fontSize: 18, marginBottom: 10, color: 'white', width: 50, backgroundColor: '#000' }}>
-                        {' '}Scan{' '}
-                    </Text>
-                </TouchableOpacity>
-
-              </View>
-            </Camera>
-          }
-          </View>
+        console.log('camera on')
       );
     }
   }
