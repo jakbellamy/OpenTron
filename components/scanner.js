8abf477d1b842b9853f44d33ff8b20ada6cd5271
Jakob Bellamy <jakbellamy@gmail.com>
2019-04-14 03:38:40 -0500

    post photo when taken

--- components/scanner.js
+++ components/scanner.js
@@ -2,13 +2,29 @@ import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import { Camera, Permissions, ImagePicker } from 'expo';
 
-
-
+const postRequest = (e, photo) => {
+  e.preventDefault()
+  console.log('hit')
+  fetch(`https://opentron.appspot.com/query`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      image: photo,
+      name: 'test.jpg'
+    })
+  })
+  .then(res => res.text())
+  .then(res => console.log("res: " + res))
+  .catch(err => console.log("error: " + err))
+}
 
 export default class Scanner extends React.Component {
     state = {
       hasCameraPermission: null,
-      type: Camera.Constants.Type.back
+      type: Camera.Constants.Type.back,
+      photo: ''
     }
   
     async componentDidMount() {
@@ -16,12 +32,14 @@ export default class Scanner extends React.Component {
       this.setState({ hasCameraPermission: permission.status === 'granted' });
     }
 
-    snap = async () => {
+    snap = async (e) => {
       if (this.camera) {
-        let photo = await this.camera.takePictureAsync();
-        console.log(photo)
-      }
-    };
+        let photo = await this.camera.takePictureAsync(options={base64: true, quality: 0.2});
+        postRequest(e, photo.base64)
+      };
+    }
+
+
 
     render() {
       const { hasCameraPermission } = this.state;
@@ -45,7 +63,7 @@ export default class Scanner extends React.Component {
                     alignSelf: 'flex-end',
                     alignItems: 'center',
                   }}
-                  onPress={() => {this.snap()}}>
+                  onPress={(e) => {this.snap(e)}}>
                   <Text
                     style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
                     Capture
