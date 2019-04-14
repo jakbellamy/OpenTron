c7880632c19904b7a70d72e7e12e1735f2f92f52
Jakob Bellamy <jakbellamy@gmail.com>
2019-04-14 01:18:41 -0500

    taking PICTURES

--- components/scanner.js
+++ components/scanner.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import { Camera, Permissions, ImagePicker } from 'expo';
 
+
+
+
 export default class Scanner extends React.Component {
-  
     state = {
       hasCameraPermission: null,
       type: Camera.Constants.Type.back
-      // isCapturing: 'false'
     }
   
     async componentDidMount() {
@@ -15,19 +16,45 @@ export default class Scanner extends React.Component {
       this.setState({ hasCameraPermission: permission.status === 'granted' });
     }
 
-    options = {
-      base64: true
-    }
-    
+    snap = async () => {
+      if (this.camera) {
+        let photo = await this.camera.takePictureAsync();
+        console.log(photo)
+      }
+    };
+
     render() {
-      ImagePicker.launchCameraAsync(this.options)
-      .then((success) => {
-        console.log("success: "+ success)
-      }, (err) => {
-        console.log("error: " + err)
-      })
-      return (
-        console.log('camera on')
-      );
+      const { hasCameraPermission } = this.state;
+      if (hasCameraPermission === null) {
+        return <View />;
+      } else if (hasCameraPermission === false) {
+        return <Text>No access to camera</Text>;
+      } else {
+        return (
+          <View style={{ flex: 1 }}>
+            <Camera ref={ref => {this.camera = ref}}style={{ flex: 1 }} type={this.state.type}>
+              <View
+                style={{
+                  flex: 1,
+                  backgroundColor: 'transparent',
+                  flexDirection: 'row',
+                }}>
+                <TouchableOpacity
+                  style={{
+                    flex: 0.1,
+                    alignSelf: 'flex-end',
+                    alignItems: 'center',
+                  }}
+                  onPress={() => {this.snap()}}>
+                  <Text
+                    style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                    Capture
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </Camera>
+          </View>
+        );
+      }
     }
   }
