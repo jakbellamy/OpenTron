4e9a74c013bfa0f1f71750844ad473ea5b9c45a0
Jakob Bellamy <jakbellamy@gmail.com>
2019-04-17 08:06:29 -0500

    and the about

--- components/about.js
+++ components/about.js
@@ -8,9 +8,24 @@ export default class About extends React.Component {
          <Card>
             <CardItem>
               <Text>
-                OpenTron is an open source alternative to expensive multiple-choice test graders. \n Press 'Set Key' tab on this main menu in order to choose from an existing key, or enter a new key by pressing the '+' button. \n Once your key is set, simply press the camera logo on the top right corner to grade any OpenTron test sheet. \n Classes feature to come later.
+                OpenTron is an open source alternative to expensive multiple-choice test graders. 
+              </Text>  
+            </CardItem>
+            <CardItem>
+              <Text>
+                Press 'Set Key' tab on this main menu in order to choose from an existing key, or enter a new key by pressing the '+' button. 
               </Text>
-            </CardItem>  
+            </CardItem>
+            <CardItem>
+              <Text>
+                Once your key is set, simply press the camera logo on the top right corner to grade any OpenTron test sheet. 
+              </Text>
+            </CardItem>
+            <CardItem>
+              <Text>
+                Classes feature to come later.
+              </Text>
+            </CardItem>
           </Card>
       </Content>
     )
